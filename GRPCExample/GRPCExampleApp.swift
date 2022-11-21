aefe582799bdb8c50182dc2041296eb0d15ae091
Vlad Maltsev <v.maltsev@thesollution.com>
2022-11-21 14:31:35 +0300

    Initial Commit

---
+++ GRPCExample/GRPCExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRPCExampleApp.swift
+//  GRPCExample
+//
+//  Created by Vladislav Maltsev on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GRPCExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
